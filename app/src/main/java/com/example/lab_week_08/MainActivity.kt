46cf5ae1020d5524b659aa7c575445bbb7f27984
ElFami <eldosan0805@gmail.com>
2025-11-24 01:12:16 +0700

    Commit No. 1 – add first & second worker

---
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.lab_week_08
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import androidx.work.Data
+import com.example.lab_week_08.worker.FirstWorker
+import com.example.lab_week_08.worker.SecondWorker
+
+class MainActivity : AppCompatActivity() {
+
+    private val workManager = WorkManager.getInstance(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val networkConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val id = "001"
+
+        val firstRequest = OneTimeWorkRequest
+            .Builder(FirstWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
+            .build()
+
+        val secondRequest = OneTimeWorkRequest
+            .Builder(SecondWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(SecondWorker.INPUT_DATA_ID, id))
+            .build()
+
+        workManager.beginWith(firstRequest)
+            .then(secondRequest)
+            .enqueue()
+
+        workManager.getWorkInfoByIdLiveData(firstRequest.id)
+            .observe(this) { info ->
+                if (info?.state?.isFinished == true) {
+                    showResult("First process is done")
+                }
+            }
+
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this) { info ->
+                if (info?.state?.isFinished == true) {
+                    showResult("Second process is done")
+                }
+            }
+
+    }
+
+    private fun getIdInputData(idKey: String, idValue: String) =
+        Data.Builder().putString(idKey, idValue).build()
+
+    private fun showResult(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    // ... rest of class (enableEdgeToEdge, companion object if any)
+}
